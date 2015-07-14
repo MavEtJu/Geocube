f5203c4708a2716910bde3450236e094e0279342
Edwin Groothuis <edwin@mavetju.org>
2015-07-14 18:06:25 +1000

    Add some future things.

---
+++ Geocube/CachesOfflineFilterViewController.h
@@ -0,0 +1,11 @@
+//
+//  CachesOfflineFilter.h
+//  Geocube
+//
+//  Created by Edwin Groothuis on 14/07/2015.
+//  Copyright (c) 2015 Edwin Groothuis. All rights reserved.
+//
+
+@interface CachesOfflineFilterViewController : GCTableViewController
+
+@end
---
+++ Geocube/CachesOfflineFilterViewController.m
@@ -0,0 +1,13 @@
+//
+//  CachesOfflineFilter.m
+//  Geocube
+//
+//  Created by Edwin Groothuis on 14/07/2015.
+//  Copyright (c) 2015 Edwin Groothuis. All rights reserved.
+//
+
+#import "Geocube-Prefix.pch"
+
+@implementation CachesOfflineFilterViewController;
+
+@end
