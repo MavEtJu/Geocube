e47d99a35054c54386476c3c2027b04976b7171e
Edwin Groothuis <edwin@mavetju.org>
2015-07-18 23:01:22 +1000

    Welcome to the GPLv3

--- Geocube/MapOSMViewController.h
+++ Geocube/MapOSMViewController.h
@@ -1,10 +1,23 @@
-//
-//  CachesOfflineOSMViewController.h
-//  Geocube
-//
-//  Created by Edwin Groothuis on 13/07/2015.
-//  Copyright (c) 2015 Edwin Groothuis. All rights reserved.
-//
+/*
+ * Geocube
+ * By Edwin Groothuis <geocube@mavetju.org>
+ * Copyright 2015 Edwin Groothuis
+ * 
+ * This file is part of Geocube.
+ * 
+ * Geocube is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Geocube is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 @interface MapOSMViewController : MapAppleViewController<MKMapViewDelegate> {
 }
