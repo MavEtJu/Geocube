6a6925693af75affc7e68985bd619d5fe784b97c
Edwin Groothuis <edwin@mavetju.org>
2015-07-26 21:39:49 +1000

    Add test module

--- tests/main.m
+++ tests/main.m
@@ -8,10 +8,41 @@
 
 #import <Foundation/Foundation.h>
 
+@interface foo : NSObject
++ (NSString *)niceNumber:(NSInteger)i;
+@end
+
+@implementation foo
+
++ (NSString *)niceNumber:(NSInteger)i
+{
+    NSMutableString *sin = [NSMutableString stringWithFormat:@"%ld", (long)i];
+    NSMutableString *sout = [NSMutableString stringWithString:@""];
+    NSInteger l = [sin length];
+    while (l > 0) {
+        if ([sout length] != 0)
+            [sout insertString:@" " atIndex:0];
+        [sout insertString:[sin substringWithRange:NSMakeRange(l > 3 ? l - 3 : 0, l > 3 ? 3 : l)] atIndex:0];
+        l -= 3;
+    }
+    return sout;
+}
+
+@end
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         NSLog(@"Hello, World!");
+
+        NSInteger i_ = 1;
+        for (NSInteger i = 0; i < 10; i++) {
+            i_ *= 10;
+            for (NSInteger j = -1; j < 2; j++) {
+                NSInteger k = i_ + j;
+                NSLog(@"%ld %@", k, [foo niceNumber:k]);
+            }
+        }
     }
     return 0;
 }
