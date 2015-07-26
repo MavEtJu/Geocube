e2799e11feb4ab52d6d7879301ce5558803f2b24
Edwin Groothuis <edwin@mavetju.org>
2015-07-26 21:39:45 +1000

    Add test module

---
+++ tests/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  tests
+//
+//  Created by Edwin Groothuis on 26/07/2015.
+//  Copyright (c) 2015 Edwin Groothuis. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
