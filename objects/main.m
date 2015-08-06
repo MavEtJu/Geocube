6e5cb29a495f3f8e2d92f97d4527fbc75978c36d
Edwin Groothuis <edwin@mavetju.org>
2015-08-06 21:57:58 +1000

    Learn how to use NSScanner

--- objects/main.m
+++ objects/main.m
@@ -7,79 +7,41 @@
 //
 
 #import <Foundation/Foundation.h>
-#import <Objc/runtime.h>
-
-@interface testObject : NSObject {
-    NSNumber *i;
-    NSString *s;
-}
-
-@property (nonatomic, retain) NSString *s;
-@property (nonatomic) NSNumber *i;
-
-@end
-
-@implementation testObject
-
-@synthesize  i, s;
-
-@end
-
-#define SuppressPerformSelectorLeakWarning(Stuff) \
-    do { \
-        _Pragma("clang diagnostic push") \
-        _Pragma("clang diagnostic ignored \"-Warc-performSelector-leaks\"") \
-        Stuff; \
-        _Pragma("clang diagnostic pop") \
-    } while (0)
 
 
 int main(int argc, const char * argv[]) {
-    testObject *t = [[testObject alloc] init];
-    t.i = @24;
-    t.s = @"foo";
-    NSLog(@"b. %@ %@", t.i, t.s);
+    NSString *a = @"E 12 30.000";
+    NSScanner *scanner = [NSScanner scannerWithString:a];
+    BOOL okay = YES;
 
-    unsigned int propertyCount = 0;
-    objc_property_t *properties = class_copyPropertyList([t class], &propertyCount);
-    NSLog(@"%d properties", propertyCount);
+    NSString *direction;
+    NSLog(@"%d/%d - scanning string - '%@'", okay, scanner.atEnd, [scanner.string substringFromIndex:scanner.scanLocation]);
+    okay &= [scanner scanCharactersFromSet:[NSCharacterSet characterSetWithCharactersInString:@"NESW"] intoString:&direction];
 
-    // http://iphonedevsdk.com/forum/iphone-sdk-development/71859-iterate-through-an-objects-properties-is-it-possible.html
+    int degrees;
+    NSLog(@"%d/%d - scanning int: '%@'", okay, scanner.atEnd, [scanner.string substringFromIndex:scanner.scanLocation]);
+    okay &= [scanner scanInt:&degrees];
 
-    for (unsigned int i = 0; i < propertyCount; i++) {
-        objc_property_t property = properties[i];
-        NSString *propertyName = [NSString stringWithFormat:@"%s", property_getName(property)];
-        NSLog(@"0. %@", propertyName);
+    float mins;
+    NSLog(@"%d/%d - scanning float: '%@'", okay, scanner.atEnd, [scanner.string substringFromIndex:scanner.scanLocation]);
+    okay &= [scanner scanFloat:&mins];
 
-        char firstLetter = [propertyName characterAtIndex:0];
-        propertyName = [propertyName substringFromIndex:1];
+    NSLog(@"%d/%d - result: '%@'", okay, scanner.atEnd, [scanner.string substringFromIndex:scanner.scanLocation]);
 
-        if (firstLetter > 96 && firstLetter < 123) {
-            firstLetter -= 32;
-        }
+    NSLog(@"Okay? %d", okay);
+    NSLog(@"input: %@", a);
+    NSLog(@"direction: %@", direction);
+    NSLog(@"degrees: %d", degrees);
+    NSLog(@"mins: %f", mins);
 
-        NSString *selectorName = [NSString stringWithFormat:@"set%c%@:", firstLetter, propertyName];
+    float ddegrees = degrees + mins / 60.0;
 
-        NSLog(@"1. %@ %@", t.i, t.s);
-        if (i == 0) {
-            SuppressPerformSelectorLeakWarning(
-                [t performSelector:NSSelectorFromString(selectorName) withObject:@"Bar"];
-            );
-        }
-        if (i == 1) {
-            NSNumber *n = @12;
-            SuppressPerformSelectorLeakWarning(
-                [t performSelector:NSSelectorFromString(selectorName) withObject:n];
-            );
-        }
-        NSLog(@"2. %@ %@", t.i, t.s);
-    }
+    if ([[direction uppercaseString] compare:@"W"] == NSOrderedSame)
+        ddegrees = -ddegrees;
+    if ([[direction uppercaseString] compare:@"S"] == NSOrderedSame)
+        ddegrees = -ddegrees;
 
-    NSLog(@"a. %@ %@", t.i, t.s);
+    NSLog(@"%f", ddegrees);
 
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
-    }
     return 0;
 }
