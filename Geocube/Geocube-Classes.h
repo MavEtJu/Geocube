391febf2907352da9e650d27bed4ef4111214702
Edwin Groothuis <edwin@mavetju.org>
2015-08-26 07:19:30 +1000

    Define all classes before importing the rest of the files.

---
+++ Geocube/Geocube-Classes.h
@@ -0,0 +1,126 @@
+/*
+ * Geocube
+ * By Edwin Groothuis <geocube@mavetju.org>
+ * Copyright 2015 Edwin Groothuis
+ *
+ * This file is part of Geocube.
+ *
+ * Geocube is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Geocube is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Geocube.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef Geocube_Geocube_Classes_h
+#define Geocube_Geocube_Classes_h
+
+@class AppDelegate;
+@class BookmarksAccountsViewController;
+@class BookmarksBrowserViewController;
+@class BookmarksUserViewController;
+@class CacheAttributesViewController;
+@class CacheDescriptionViewController;
+@class CacheFilterManager;
+@class CacheGroupsViewController;
+@class CacheHeaderTableViewCell;
+@class CacheHintViewController;
+@class CacheImageViewController;
+@class CacheImagesViewController;
+@class CacheLogsViewController;
+@class CachePersonalNoteViewController;
+@class CacheTableViewCell;
+@class CacheTravelbugsViewController;
+@class CacheViewController;
+@class CacheWaypointsViewController;
+@class CachesOfflineListViewController;
+@class CompassViewController;
+@class Coordinates;
+@class FilesViewController;
+@class FilterCategoryTableViewCell;
+@class FilterDateTableViewCell;
+@class FilterDifficultyTableViewCell;
+@class FilterDirectionTableViewCell;
+@class FilterDistanceTableViewCell;
+@class FilterFavouritesTableViewCell;
+@class FilterGroupsTableViewCell;
+@class FilterObject;
+@class FilterOthersTableViewCell;
+@class FilterSizesTableViewCell;
+@class FilterTableViewCell;
+@class FilterTerrainTableViewCell;
+@class FilterTextTableViewCell;
+@class FilterTypesTableViewCell;
+@class FiltersViewController;
+@class GCLocationManager;
+@class GCOAuthBlackbox;
+@class GCPointAnnotation;
+@class GCTableViewCell;
+@class GCTableViewCellRightImage;
+@class GCTableViewCellTwoTextfields;
+@class GCTableViewCellWithSubtitle;
+@class GCTableViewController;
+@class GCViewController;
+@class GlobalMenu;
+@class GroupsViewController;
+@class HelpAboutViewController;
+@class HelpDatabaseViewController;
+@class HelpImagesViewController;
+@class ImageLibrary;
+@class ImagesDownloadManager;
+@class ImportGPX;
+@class ImportGPXViewController;
+@class ImportLicenses;
+@class LiveAPI;
+@class LogTableViewCell;
+@class MapAppleViewController;
+@class MapGoogleViewController;
+@class MapOSMViewController;
+@class MapTemplateViewController;
+@class MyConfig;
+@class MyTools;
+@class NotesFieldnotesViewController;
+@class NotesImagesViewController;
+@class NotesPersonalNotesViewController;
+@class NullViewController;
+@class NullViewController;
+@class OKAPI;
+@class PersonalNoteTableViewCell;
+@class SettingsAccountsViewController;
+@class SettingsMainViewController;
+@class ThemeGeosphere;
+@class ThemeNormal;
+@class ThemeTemplate;
+@class TravelbugsViewController;
+@class UserProfileViewController;
+@class DatabaseCache;
+@class database;
+@class dbAccount;
+@class dbAttribute;
+@class dbBookmark;
+@class dbConfig;
+@class dbContainer;
+@class dbCountry;
+@class dbFilter;
+@class dbGroundspeak;
+@class dbGroup;
+@class dbImage;
+@class dbLog;
+@class dbLogType;
+@class dbName;
+@class dbObject;
+@class dbPersonalNote;
+@class dbState;
+@class dbSymbol;
+@class dbTravelbug;
+@class dbType;
+@class dbWaypoint;
+
+#endif
