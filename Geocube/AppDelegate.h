4aefea424dca31972963b4407d12aa52a60d0214
Edwin Groothuis <edwin@mavetju.org>
2015-09-03 21:21:00 +1000

    Remove @class from normal include files

--- Geocube/AppDelegate.h
+++ Geocube/AppDelegate.h
@@ -19,8 +19,6 @@
  * along with Geocube.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-@class JASidePanelController;
-
 @interface AppDelegate : UIResponder <UITabBarControllerDelegate> {
     NSMutableArray *tabBars;
 }
