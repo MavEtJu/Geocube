a7e7b01e35009e3339b06bf7a3b690d9274ba816
Edwin Groothuis <edwin@mavetju.org>
2015-09-16 07:39:04 +1000

    Make cache logs not mess up anymore with regard to size

--- Geocube/NotesFieldnotesViewController.m
+++ Geocube/NotesFieldnotesViewController.m
@@ -88,12 +88,11 @@
 
     cell.datetime.text = [NSString stringWithFormat:@"%@ %@", [MyTools datetimePartDate:l.datetime], [MyTools datetimePartTime:l.datetime]];
     cell.logger.text = l.logger.name;
-    cell.log.text = l.log;
     cell.log.lineBreakMode = NSLineBreakByWordWrapping;
     dbLogType *lt = [dbc LogType_get:l.logtype_id];
     cell.logtype.image = [imageLibrary get:lt.icon];
 
-    [cell.log sizeToFit];
+    [cell setLogString:l.log];
     [cell.contentView sizeToFit];
     [cell setUserInteractionEnabled:NO];
 
