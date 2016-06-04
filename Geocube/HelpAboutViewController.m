eb7f50fb2986026cb05dd383b694d7401553434d
Edwin Groothuis <edwin@mavetju.org>
2016-06-04 17:20:54 +1000

    Add credits for VKSideMenu

--- Geocube/HelpAboutViewController.m
+++ Geocube/HelpAboutViewController.m
@@ -159,6 +159,13 @@
               @"SSZipArchive: Copyright (c) 2010-2015, Sam Soffes, http://soff.es\n"
               "(https://github.com/iosphere/ssziparchive)",
 
+              @"VKSideMenu: Copyright © 2016 WOOPSS.com (http://woopss.com/)\n"
+              "https://github.com/vladislav-k/VKSideMenu\n"
+              "Created by Vladislav Kovalyov on 2/7/16.\n"
+              "Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the \"Software\"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:\n"
+              "The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.\n"
+              "THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.",
+
               @"YIPopupTextView: Copyright (c) 2012 Yasuhiro Inami. All rights reserved.\n"
               "(https://github.com/inamiy/YIPopupTextView)\n"
               "`YIPopupTextView` is available under the [Beerware](http://en.wikipedia.org/wiki/Beerware) license. If we meet some day, and you think this stuff is worth it, you can buy me a beer in return.",
