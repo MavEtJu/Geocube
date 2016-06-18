876d623162eb71f8664cdae7a0676ab52d09c4de
Edwin Groothuis <edwin@mavetju.org>
2016-06-18 13:09:09 +1000

    Add options to sort on name and code

--- Geocube/WaypointsOfflineListViewController.m
+++ Geocube/WaypointsOfflineListViewController.m
@@ -59,6 +59,10 @@ enum {
     SORTORDER_TERRAIN_DESC,
     SORTORDER_DIFFICULTY_ASC,
     SORTORDER_DIFFICULTY_DESC,
+    SORTORDER_NAME_ASC,
+    SORTORDER_NAME_DESC,
+    SORTORDER_CODE_ASC,
+    SORTORDER_CODE_DESC,
     SORTORDER_DATE_FOUND_OLDESTFIRST,
     SORTORDER_DATE_FOUND_NEWESTFIRST,
     SORTORDER_DATE_LASTLOG_OLDESTFIRST,
@@ -189,7 +193,7 @@ enum {
         wp.calculatedBearing = [Coordinates coordinates2bearing:wp.coordinates to:LM.coords];
     }];
 
-#define CMP(I, O1, O2, W) \
+#define NCMP(I, O1, O2, W) \
     case I: \
         wps = [wps sortedArrayUsingComparator: ^(dbWaypoint *obj1, dbWaypoint *obj2) { \
             if (O1 W O2) \
@@ -199,25 +203,38 @@ enum {
             return (NSComparisonResult)NSOrderedSame; \
         }]; \
         break;
+#define SCMP(I, O1, O2, W) \
+    case I: \
+        wps = [wps sortedArrayUsingComparator: ^(dbWaypoint *obj1, dbWaypoint *obj2) { \
+            if (W == NSOrderedAscending) \
+                return (NSComparisonResult)[O1 compare:O2 options:NSCaseInsensitiveSearch]; \
+            else \
+                return (NSComparisonResult)(-[O1 compare:O2 options:NSCaseInsensitiveSearch]); \
+        }]; \
+        break;
     switch (currentSortOrder) {
-        CMP(SORTORDER_DISTANCE_ASC, obj1.calculatedDistance, obj2.calculatedDistance, >)
-        CMP(SORTORDER_DISTANCE_DESC, obj1.calculatedDistance, obj2.calculatedDistance, <)
-        CMP(SORTORDER_DIRECTION_ASC, obj1.calculatedBearing, obj2.calculatedBearing, <)
-        CMP(SORTORDER_DIRECTION_DESC, obj1.calculatedBearing, obj2.calculatedBearing, >)
-        CMP(SORTORDER_TYPE, obj1.wpt_type_id, obj2.wpt_type_id, >)
-        CMP(SORTORDER_CONTAINER, obj1.gs_container_id, obj2.gs_container_id, >)
-        CMP(SORTORDER_FAVOURITES_ASC, obj1.gs_favourites, obj2.gs_favourites, >)
-        CMP(SORTORDER_FAVOURITES_DESC, obj1.gs_favourites, obj2.gs_favourites, <)
-        CMP(SORTORDER_TERRAIN_ASC, obj1.gs_rating_terrain, obj2.gs_rating_terrain, >)
-        CMP(SORTORDER_TERRAIN_DESC, obj1.gs_rating_terrain, obj2.gs_rating_terrain, <)
-        CMP(SORTORDER_DIFFICULTY_ASC, obj1.gs_rating_difficulty, obj2.gs_rating_difficulty, >)
-        CMP(SORTORDER_DIFFICULTY_DESC, obj1.gs_rating_difficulty, obj2.gs_rating_difficulty, <)
-        CMP(SORTORDER_DATE_HIDDEN_OLDESTFIRST, obj1.wpt_date_placed_epoch, obj2.wpt_date_placed_epoch, >)
-        CMP(SORTORDER_DATE_HIDDEN_NEWESTFIRST, obj1.wpt_date_placed_epoch, obj2.wpt_date_placed_epoch, <)
-        CMP(SORTORDER_DATE_FOUND_OLDESTFIRST, obj1.gs_date_found, obj2.gs_date_found, >)
-        CMP(SORTORDER_DATE_FOUND_NEWESTFIRST, obj1.gs_date_found, obj2.gs_date_found, <)
-        CMP(SORTORDER_DATE_LASTLOG_OLDESTFIRST, obj1.date_lastlog_epoch, obj2.date_lastlog_epoch, >)
-        CMP(SORTORDER_DATE_LASTLOG_NEWESTFIRST, obj1.date_lastlog_epoch, obj2.date_lastlog_epoch, <)
+        NCMP(SORTORDER_DISTANCE_ASC, obj1.calculatedDistance, obj2.calculatedDistance, >)
+        NCMP(SORTORDER_DISTANCE_DESC, obj1.calculatedDistance, obj2.calculatedDistance, <)
+        NCMP(SORTORDER_DIRECTION_ASC, obj1.calculatedBearing, obj2.calculatedBearing, <)
+        NCMP(SORTORDER_DIRECTION_DESC, obj1.calculatedBearing, obj2.calculatedBearing, >)
+        NCMP(SORTORDER_TYPE, obj1.wpt_type_id, obj2.wpt_type_id, >)
+        NCMP(SORTORDER_CONTAINER, obj1.gs_container_id, obj2.gs_container_id, >)
+        NCMP(SORTORDER_FAVOURITES_ASC, obj1.gs_favourites, obj2.gs_favourites, >)
+        NCMP(SORTORDER_FAVOURITES_DESC, obj1.gs_favourites, obj2.gs_favourites, <)
+        NCMP(SORTORDER_TERRAIN_ASC, obj1.gs_rating_terrain, obj2.gs_rating_terrain, >)
+        NCMP(SORTORDER_TERRAIN_DESC, obj1.gs_rating_terrain, obj2.gs_rating_terrain, <)
+        NCMP(SORTORDER_DIFFICULTY_ASC, obj1.gs_rating_difficulty, obj2.gs_rating_difficulty, >)
+        NCMP(SORTORDER_DIFFICULTY_DESC, obj1.gs_rating_difficulty, obj2.gs_rating_difficulty, <)
+        SCMP(SORTORDER_CODE_ASC, obj1.wpt_name, obj2.wpt_name, NSOrderedAscending)
+        SCMP(SORTORDER_CODE_DESC, obj1.wpt_name, obj2.wpt_name, NSOrderedDescending)
+        SCMP(SORTORDER_NAME_ASC, obj1.wpt_urlname, obj2.wpt_urlname, NSOrderedAscending)
+        SCMP(SORTORDER_NAME_DESC, obj1.wpt_urlname, obj2.wpt_urlname, NSOrderedDescending)
+        NCMP(SORTORDER_DATE_HIDDEN_OLDESTFIRST, obj1.wpt_date_placed_epoch, obj2.wpt_date_placed_epoch, >)
+        NCMP(SORTORDER_DATE_HIDDEN_NEWESTFIRST, obj1.wpt_date_placed_epoch, obj2.wpt_date_placed_epoch, <)
+        NCMP(SORTORDER_DATE_FOUND_OLDESTFIRST, obj1.gs_date_found, obj2.gs_date_found, >)
+        NCMP(SORTORDER_DATE_FOUND_NEWESTFIRST, obj1.gs_date_found, obj2.gs_date_found, <)
+        NCMP(SORTORDER_DATE_LASTLOG_OLDESTFIRST, obj1.date_lastlog_epoch, obj2.date_lastlog_epoch, >)
+        NCMP(SORTORDER_DATE_LASTLOG_NEWESTFIRST, obj1.date_lastlog_epoch, obj2.date_lastlog_epoch, <)
         default:
             NSAssert(NO, @"Unknown sort order");
     }
@@ -377,6 +394,10 @@ enum {
             CASE(SORTORDER_TERRAIN_DESC, @"Terrain (descending)")
             CASE(SORTORDER_DIFFICULTY_ASC, @"Difficulty (ascending)")
             CASE(SORTORDER_DIFFICULTY_DESC, @"Difficulty (descending)")
+            CASE(SORTORDER_NAME_ASC, @"Name (ascending)")
+            CASE(SORTORDER_NAME_DESC, @"Name (descending)")
+            CASE(SORTORDER_CODE_ASC, @"Code (ascending)")
+            CASE(SORTORDER_CODE_DESC, @"Code (descending)")
             CASE(SORTORDER_DATE_LASTLOG_OLDESTFIRST, @"Last log date (oldest first)")
             CASE(SORTORDER_DATE_LASTLOG_NEWESTFIRST, @"Last log date (newest first)")
             CASE(SORTORDER_DATE_HIDDEN_OLDESTFIRST, @"Hidden date (oldest first)")
