b4d6bde12776d3fabcdb69093e4a4efd0094dabd
Edwin Groothuis <edwin@mavetju.org>
2016-10-10 22:47:49 +1100

    Properly finish the dbWaypoint object before using it

--- Geocube/Coordinates.m
+++ Geocube/Coordinates.m
@@ -395,6 +395,7 @@
             wp.wpt_type_id = [dbc Type_ManuallyEntered]._id;
             wp.related_id = waypoint._id;
             wp.account_id = waypoint.account_id;
+            [wp finish];
             [dbWaypoint dbCreate:wp];
 
             [dbc.Group_AllWaypoints_ManuallyAdded dbAddWaypoint:wp._id];
