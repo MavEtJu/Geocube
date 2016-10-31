8285fa598231083c53683d0d293f97f7c2233061
Edwin Groothuis <edwin@mavetju.org>
2016-10-31 16:43:24 +1100

    Fix SQL statement

--- Geocube/dbAccount.m
+++ Geocube/dbAccount.m
@@ -336,7 +336,7 @@
     dbAccount *a = nil;
 
     @synchronized(db.dbaccess) {
-        DB_PREPARE(@"select id, site, url_site, url_queries, accountname, protocol, oauth_consumer_public, oauth_consumer_private, oauth_token, oauth_token_secret, oauth_request_url, oauth_authorize_url, oauth_access_url, gca_cookie_name, gca_authenticate_url, gca_callback_url, geocube_id, revision, gca_cookie_value, name_id, enabled, distance_minimum from accounts, authentication_name, authentication_password where site = ?");
+        DB_PREPARE(@"select id, site, url_site, url_queries, accountname, protocol, oauth_consumer_public, oauth_consumer_private, oauth_token, oauth_token_secret, oauth_request_url, oauth_authorize_url, oauth_access_url, gca_cookie_name, gca_authenticate_url, gca_callback_url, geocube_id, revision, gca_cookie_value, name_id, enabled, distance_minimum, authentication_name, authentication_password from accounts where site = ?");
         SET_VAR_TEXT(1, site);
 
         DB_IF_STEP {
