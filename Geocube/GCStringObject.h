a9d3717533db79cda53e54b471fa5f9b2a0d6e69
Edwin Groothuis <edwin@mavetju.org>
2016-11-05 15:14:27 +1100

    Better dealing with GCString related types

--- Geocube/GCStringObject.h
+++ Geocube/GCStringObject.h
@@ -19,7 +19,7 @@
  * along with Geocube.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-@interface GCStringObject : NSString
+@interface GCStringObject : NSObject
 
 - (instancetype)initWithString:(NSString *)s;
 - (instancetype)initWithData:(NSData *)data encoding:(NSStringEncoding)encoding;
