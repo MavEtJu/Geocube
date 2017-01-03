a7e4204f1f1488846e40cafe8bd4be4375f032e5
Edwin Groothuis <edwin@mavetju.org>
2017-01-03 16:17:39 +1100

    Remove outdated delegate

--- Geocube/MapAllWPViewController.h
+++ Geocube/MapAllWPViewController.h
@@ -19,6 +19,6 @@
  * along with Geocube.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-@interface MapAllWPViewController : MapTemplateViewController <RemoteAPIRetrieveQueryDelegate, RemoteAPILoadWaypointsByBoundingBoxDelegate>
+@interface MapAllWPViewController : MapTemplateViewController <RemoteAPILoadWaypointsByBoundingBoxDelegate>
 
 @end
