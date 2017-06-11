ee21781af9b6e5f6917d8bd00e1d4f4c59538c87
Edwin Groothuis <edwin@mavetju.org>
2017-06-11 10:51:08 +1000

    Add encryption stuff for generating encrypted shared secrets

--- Geocube/main.m
+++ Geocube/main.m
@@ -68,11 +68,33 @@ KeyManager *keyManager = nil;
 //
 AppDelegate *_AppDelegate;
 
+void encryptionstuff(void);
 int main(int argc, char * argv[])
 {
     srand((unsigned int)time(NULL));
 
+    // This is only needed when generating shared secret encrypted keys
+    // encryptionstuff();
+
     @autoreleasepool {
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
+}
+
+void encryptionstuff(void)
+{
+    keyManager = [[KeyManager alloc] init];
+
+    NSString *key = @"sites_1";
+    NSArray<NSString *> *plains = @[
+        @"Foo bar quux",
+        // Your plain text goes here.
+        ];
+
+    [plains enumerateObjectsUsingBlock:^(NSString * _Nonnull plain, NSUInteger idx, BOOL * _Nonnull stop) {
+        NSLog(@"%@", [keyManager decrypt:key data:[keyManager encrypt:key data:plain]]);
+        NSLog(@"%@", [keyManager encrypt:key data:plain]);
+    }];
+
+    exit(0);
 }
