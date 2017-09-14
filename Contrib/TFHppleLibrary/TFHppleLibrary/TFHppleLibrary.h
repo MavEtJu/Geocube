777ac1b694f328814f4a19214309ae308c7fda67
Edwin Groothuis <edwin@mavetju.org>
2017-09-14 19:09:41 +1000

    Add TFHppleLibrary

---
+++ Contrib/TFHppleLibrary/TFHppleLibrary/TFHppleLibrary.h
@@ -0,0 +1,11 @@
+//
+//  TFHppleLibrary.h
+//  TFHppleLibrary
+//
+//  Created by Edwin Groothuis on 14/9/17.
+//  Copyright © 2017 Edwin Groothuis. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "TFHppleLibrary/TFHpple.h"
