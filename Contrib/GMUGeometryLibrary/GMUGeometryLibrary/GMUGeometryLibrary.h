076cee724e2ff169f14dbac738d7ff2960d6ee43
Edwin Groothuis <edwin@mavetju.org>
2017-09-14 14:42:27 +1000

    Make Google Map Utils a library.

---
+++ Contrib/GMUGeometryLibrary/GMUGeometryLibrary/GMUGeometryLibrary.h
@@ -0,0 +1,12 @@
+//
+//  GMUGeometryLibrary.h
+//  GMUGeometryLibrary
+//
+//  Created by Edwin Groothuis on 14/9/17.
+//  Copyright © 2017 Edwin Groothuis. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import <GMUGeometryLibrary/GMUKMLParser.h>
+#import <GMUGeometryLibrary/GMUGeometryRenderer.h>
