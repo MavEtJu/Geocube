3bcacb0e201a8f78109f5760f6c87cd802985df2
Edwin Groothuis <edwin@mavetju.org>
2017-09-14 19:41:16 +1000

    Add library for SSZipArchive

---
+++ Contrib/SSZipArchiveLibrary/SSZipArchiveLibrary/SSZipArchiveLibrary.h
@@ -0,0 +1,11 @@
+//
+//  SSZipArchiveLibrary.h
+//  SSZipArchiveLibrary
+//
+//  Created by Edwin Groothuis on 14/9/17.
+//  Copyright © 2017 Edwin Groothuis. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "SSZipArchiveLibrary/SSZipArchive.h"
