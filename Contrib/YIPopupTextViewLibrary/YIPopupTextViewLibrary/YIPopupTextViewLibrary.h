3684dce2f8e8dbab49af13c5693bc6277ef7c402
Edwin Groothuis <edwin@mavetju.org>
2017-09-14 19:16:35 +1000

    Add library for YIPopupTextViewLibrary

---
+++ Contrib/YIPopupTextViewLibrary/YIPopupTextViewLibrary/YIPopupTextViewLibrary.h
@@ -0,0 +1,11 @@
+//
+//  YIPopupTextViewLibrary.h
+//  YIPopupTextViewLibrary
+//
+//  Created by Edwin Groothuis on 14/9/17.
+//  Copyright © 2017 Edwin Groothuis. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import <YIPopupTextViewLibrary/YIPopupTextView.h>
