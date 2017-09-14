a95e70249816ace46ff0dc55e6c5dfe15304346d
Edwin Groothuis <edwin@mavetju.org>
2017-09-14 15:16:03 +1000

    Make NVHTarGzip a library.

---
+++ Contrib/NVHTarGzipLibrary/NVHTarGzipLibrary/NVHTarGzipLibrary.h
@@ -0,0 +1,11 @@
+//
+//  NVHTarGzipLibrary.h
+//  NVHTarGzipLibrary
+//
+//  Created by Edwin Groothuis on 14/9/17.
+//  Copyright © 2017 Edwin Groothuis. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import <NVHTarGzipLibrary/NVHTarGzip.h>
---
+++ Contrib/TouchXMLLibrary/TouchXMLLibrary/TouchXMLLibrary.h
@@ -0,0 +1,11 @@
+//
+//  TouchXMLLibrary.h
+//  TouchXMLLibrary
+//
+//  Created by Edwin Groothuis on 14/9/17.
+//  Copyright © 2017 Edwin Groothuis. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import <TouchXMLLibrary/TouchXML.h>
