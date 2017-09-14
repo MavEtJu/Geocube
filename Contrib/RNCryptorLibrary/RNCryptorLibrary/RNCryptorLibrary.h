8a2d5eb47d70b64da88a779e2a31d857910d02aa
Edwin Groothuis <edwin@mavetju.org>
2017-09-14 14:09:37 +1000

    Make a library of RNCryptor.

---
+++ Contrib/RNCryptorLibrary/RNCryptorLibrary/RNCryptorLibrary.h
@@ -0,0 +1,13 @@
+//
+//  RNCryptorLibrary.h
+//  RNCryptorLibrary
+//
+//  Created by Edwin Groothuis on 14/9/17.
+//  Copyright © 2017 Edwin Groothuis. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import <RNCryptorLibrary/RNCryptor.h>
+#import <RNCryptorLibrary/RNEncryptor.h>
+#import <RNCryptorLibrary/RNDecryptor.h>
