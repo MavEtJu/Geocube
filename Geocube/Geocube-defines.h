0511a114b07570fb6d64610fe095657dd1425c9c
Edwin Groothuis <edwin@mavetju.org>
2017-09-15 15:18:41 +1000

    Move some defines around.

--- Geocube/Geocube-defines.h
+++ Geocube/Geocube-defines.h
@@ -19,9 +19,6 @@
  * along with Geocube.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef Geocube_defines_h
-#define Geocube_defines_h
-
 #define ASSERT_FINISHED \
     NSAssert(finished == YES, @"Not finished");
 #define ASSERT_SELF_FIELD_EXISTS(__field__) \
@@ -32,4 +29,142 @@
 #define ASSERT_IMPORT(__expression__, __message__) \
     NSAssert(__expression__, __message__)
 
-#endif /* Geocube_defines_h */
+#define IS_IPAD \
+    (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad)
+#define IS_EMPTY(__s__) \
+    (__s__ == nil || [__s__ length] == 0)
+
+#define NEEDS_OVERLOADING_ASSERT \
+    NSAssert(0, @"%s should be overloaded for %@", __FUNCTION__, [self class])
+#define NEEDS_OVERLOADING_VOID(__name__) \
+    (void) __name__ { NEEDS_OVERLOADING_ASSERT; }
+
+#define NEEDS_OVERLOADING_NIL(__type__, __name__) \
+(__type__) __name__ { NEEDS_OVERLOADING_ASSERT; return nil; }
+
+#define NEEDS_OVERLOADING_NSSTRING(__name__) \
+    NEEDS_OVERLOADING_NIL(NSString *, __name__)
+#define NEEDS_OVERLOADING_NSARRAY_DBOBJECT(__name__) \
+    NEEDS_OVERLOADING_NIL(NSArray<dbObject *> *, __name__)
+#define NEEDS_OVERLOADING_DBOBJECT(__name__) \
+    NEEDS_OVERLOADING_NIL(dbObject *, __name__)
+#define NEEDS_OVERLOADING_NSARRAY_NSSTRING(__name__) \
+    NEEDS_OVERLOADING_NIL(NSArray<NSString *> *, __name__)
+#define NEEDS_OVERLOADING_NSDICTIONARY(__name__) \
+    NEEDS_OVERLOADING_NIL(NSDictionary *, __name__)
+#define NEEDS_OVERLOADING_INSTANCETYPE(__name__) \
+    NEEDS_OVERLOADING_NIL(instancetype, __name__)
+
+#define NEEDS_OVERLOADING_BOOL(__name__) \
+    (BOOL) __name__ { NEEDS_OVERLOADING_ASSERT; return NO; }
+#define NEEDS_OVERLOADING_NSRANGE(__name__) \
+    (NSRange) __name__ { NEEDS_OVERLOADING_ASSERT; return NSMakeRange(0, 0); }
+#define NEEDS_OVERLOADING_CLLOCATIONCOORDINATE2D(__name__) \
+    (CLLocationCoordinate2D) __name__ { NEEDS_OVERLOADING_ASSERT; return CLLocationCoordinate2DZero; }
+#define NEEDS_OVERLOADING_NSINTEGER(__name__) \
+    (NSInteger) __name__ { NEEDS_OVERLOADING_ASSERT; return 0; }
+#define NEEDS_OVERLOADING_GCMAPTYPE(__name__) \
+    (GCMapType) __name__ { NEEDS_OVERLOADING_ASSERT; return 0; }
+#define NEEDS_OVERLOADING_DOUBLE(__name__) \
+    (double) __name__ { NEEDS_OVERLOADING_ASSERT; return 0; }
+#define NEEDS_OVERLOADING_NSID(__name__) \
+    (NSId) __name__ { NEEDS_OVERLOADING_ASSERT; return 0; }
+
+#define EMPTY_METHOD(__name__) \
+    - (void) __name__ { }
+#define EMPTY_METHOD_DOUBLE(__name__) \
+    - (double) __name__ { return 0; }
+#define EMPTY_METHOD_BOOL(__name__) \
+    - (BOOL) __name__ { return NO; }
+
+// JSON related safety functions
+#define DICT_NSSTRING_KEY(__dict__, __a__, __key__) { \
+    NSString *__b__ = [__dict__ objectForKey:__key__]; \
+    if ([__b__ isKindOfClass:[NSNull class]] == TRUE) \
+        __b__ = @""; \
+    if ([__b__ isKindOfClass:[NSNumber class]] == YES) { \
+        __a__ = [(NSNumber *)__b__ stringValue]; \
+    } else \
+        __a__ = __b__; \
+    }
+#define DICT_NSSTRING_PATH(__dict__, __a__, __path__) { \
+    NSString *__b__ = [__dict__ valueForKeyPath:__path__]; \
+    if ([__b__ isKindOfClass:[NSNull class]] == TRUE) \
+        __b__ = @""; \
+    if ([__b__ isKindOfClass:[NSNumber class]] == YES) \
+        __a__ = [(NSNumber *)__b__ stringValue]; \
+    else \
+        __a__ = __b__; \
+}
+
+#define DICT_FLOAT_KEY(__dict__, __a__, __key__) { \
+    NSString *__b__ = [__dict__ objectForKey:__key__]; \
+    if ([__b__ isKindOfClass:[NSNull class]] == TRUE) \
+        __b__ = @"0"; \
+    __a__ = [__b__  floatValue]; \
+}
+#define DICT_FLOAT_PATH(__dict__, __a__, __path__) { \
+    NSString *__b__ = [__dict__ valueForKeyPath:__path__]; \
+    if ([__b__ isKindOfClass:[NSNull class]] == TRUE) \
+        __b__ = @"0"; \
+    __a__ = [__b__  floatValue]; \
+}
+
+#define DICT_INTEGER_KEY(__dict__, __a__, __key__) { \
+NSString *__b__ = [__dict__ objectForKey:__key__]; \
+    if ([__b__ isKindOfClass:[NSNull class]] == TRUE) \
+        __b__ = @"0"; \
+    __a__ = [__b__  integerValue]; \
+}
+#define DICT_INTEGER_PATH(__dict__, __a__, __path__) { \
+    NSString *__b__ = [__dict__ valueForKeyPath:__path__]; \
+    if ([__b__ isKindOfClass:[NSNull class]] == TRUE) \
+        __b__ = @"0"; \
+    __a__ = [__b__  integerValue]; \
+}
+
+#define DICT_BOOL_KEY(__dict__, __a__, __key__) { \
+    NSString *__b__ = [__dict__ objectForKey:__key__]; \
+    if ([__b__ isKindOfClass:[NSNull class]] == TRUE) \
+        __b__ = @"0"; \
+    __a__ = [__b__  boolValue]; \
+}
+#define DICT_BOOL_PATH(__dict__, __a__, __path__) { \
+    NSString *__b__ = [__dict__ valueForKeyPath:__path__]; \
+    if ([__b__ isKindOfClass:[NSNull class]] == TRUE) \
+        __b__ = @"0"; \
+    __a__ = [__b__  boolValue]; \
+}
+
+#define DICT_ARRAY_KEY(__dict__, __a__, __key__) { \
+    NSArray*__b__ = [__dict__ objectForKey:__key__]; \
+    if ([__b__ isKindOfClass:[NSNull class]] == TRUE) \
+        __b__ = @[]; \
+    __a__ = __b__; \
+}
+#define DICT_ARRAY_PATH(__dict__, __a__, __path__) { \
+    NSArray*__b__ = [__dict__ valueForKeyPath:__path__]; \
+    if ([__b__ isKindOfClass:[NSNull class]] == TRUE) \
+        __b__ = @[]; \
+    __a__ = __b__; \
+}
+
+// UIAlertController related macro
+#define ALERT_VC_RVC(__vc__) \
+    __vc__.view.window.rootViewController
+
+// Logging macro
+
+#define GCLog(__fmt__, ...) { \
+    NSString *fmt = [NSString stringWithFormat:@"%%s: %@", __fmt__]; \
+    NSLog(fmt, __func__, ## __VA_ARGS__); \
+}
+
+// Main Thread macro
+#define MAINQUEUE(block) \
+    [[NSOperationQueue mainQueue] addOperationWithBlock:^{ \
+        block; \
+    }]; \
+
+#define BACKGROUND(__selector__, __data__) \
+    [self performSelectorInBackground:@selector(__selector__) withObject:__data__];
